5cba8a34d5ce66fabf09d74800e6c2edd23a391e
uma821 <uma.ohmoto@gmail.com>
2019-07-31 20:38:56 +0900

    :sparkles: フラグ機能を追加しました！(ver2.2)
    フラグを立てて見やすくできる機能を実装しました！

--- src/main.c
+++ src/main.c
@@ -1,30 +1,39 @@
-//マインスイーパー2.1.0
+//マインスイーパー2.2.0
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 int neta(int);
+void op(void);
 void opensel(int ,int);
-int map[9999][9999][3], wide, high;
+int map[9999][9999][4], wide, high;
 int main(void){
-    int x, y, ranx, rany, count = 0, xs, ys, bomb, bombs, n, fin=0;
-    printf("盤面の高さを入力＝＞　");
-    fflush(stdout);
-    scanf("%d", &high);
-    printf("盤面の幅を入力＝＞　");
-    fflush(stdout);
-    scanf("%d", &wide);
+    int x, y, ranx, rany, count = 0, xs, ys, bomb, bombs, n, fin = 0;
+    wide = high = bombs = -1;
+
+    do {
+        printf("盤面の高さを入力＝＞　");
+        fflush(stdout);
+        scanf("%d", &high);
+        while(getchar() != '\n');
+    }while(high < 0 || high > 9999);
+    do {
+        printf("盤面の幅を入力＝＞　");
+        fflush(stdout);
+        scanf("%d", &wide);
+        while(getchar() != '\n');
+    }while(wide < 0 || wide > 9999);
 
     //配列初期化
      for(y = 0; y <= high + 1; y++) {
-        for(x = 0; x <= wide + 1; x++) {
-            map[x][y][0] = map[x][y][1] = -1;
-        }
+        for(x = 0; x <= wide + 1; x++)
+            map[x][y][0] = map[x][y][1] = map[x][y][3] = -1;
     }
 
     do{
-        printf("爆弾の数(%d個以内)を入力＝＞　",high*wide);
+        printf("爆弾の数(%d個以内)を入力＝＞　",high * wide);
         fflush(stdout);
         scanf("%d", &bombs);
+        while(getchar() != '\n');
     }while(bombs > high*wide || bombs < 0);
 
     //爆弾セット
@@ -37,7 +46,6 @@ int main(void){
             x++;
         }
     }
-
 
     //場所のまわりの爆弾を数える
     for (ys = 1; ys <= high; ys++){
@@ -69,14 +77,11 @@ int main(void){
         }
     }
 
-    //neta(1);
-    puts("");
-
+    //neta(1);puts("");
 
     //ゲーム開始
     while (1) {
-        n = 0;
-         //現在の結果を画面に表示
+        //現在の結果を画面に表示
         n = neta(0);
 
         if(bombs == n){
@@ -85,10 +90,17 @@ int main(void){
         }
 
          //プレーヤーが場所を入力
-        count++;
-        printf("場所を入力してください (x y) [%d 回目]:", count);
-        fflush(stdout);
-        scanf("%d %d",&xs ,&ys);
+        do {
+            xs = ys = 0;
+            count++;
+            printf("場所を入力してください (x y) [%d 回目]:", count);
+            fflush(stdout);
+            scanf("%d %d",&xs ,&ys);
+            while(getchar() != '\n');
+        }while(xs < 0 || xs > wide || ys < 0 || ys > high);
+
+        if(xs == 0 && ys == 0)
+            op();
 
          //爆弾を引いた
         if (map[xs][ys][0] == -2)
@@ -107,6 +119,27 @@ int main(void){
         fflush(stdout);
     }
     return 0;
+}
+
+void op(void){
+    int x, y, a;
+    do {
+        x = y = 0;
+        printf("Flagの場所を入力してください (x y) :");
+        fflush(stdout);
+        scanf("%d %d",&x ,&y);
+        while(getchar() != '\n');
+    }while(x < 1 || x > wide || y < 1 || y > high);
+    do {
+        printf("0？1？　");
+        fflush(stdout);
+        scanf("%d",&a);
+        while(getchar() != '\n');
+    }while(a <  0 || a > 1);
+    if(a == 1)
+        map[x][y][3] = 0;
+    else
+        map[x][y][3] = 1;
 }
 
 int neta(int s){
@@ -140,9 +173,11 @@ int neta(int s){
                 else if(map[x][y][2] == 8)
                     printf("\033[038;2;127;127;127m");
                 printf("%d \033[m",map[x][y][2]);
-            } else if (s == 0) {
+            } else if (s == 0 && map[x][y][3]!=0) {
                 printf("  ");
                 n++;
+            } else if (map[x][y][3]==0){
+                printf("F ");
             } else if (s == 1){
                 if(map[x][y][0] == -2)
                     printf("B ");
