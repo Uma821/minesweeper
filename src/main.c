2f79f6b773bfb7c5cbc568ba874d082ab826f99c
uma821 <uma.ohmoto@gmail.com>
2019-07-31 19:50:59 +0900

    :sparkles: 盤面の表示を別関数に移動しました(ver2.1)
    盤面を表示する関数をmain関数からneta関数に移動して、見やすくしました

--- src/main.c
+++ src/main.c
@@ -1,167 +1,165 @@
-//マインスイーパー2.0.2
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <time.h> 
+//マインスイーパー2.1.0
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int neta(int);
 void opensel(int ,int);
 int map[9999][9999][3], wide, high;
-int main(void){ 
+int main(void){
     int x, y, ranx, rany, count = 0, xs, ys, bomb, bombs, n, fin=0;
     printf("盤面の高さを入力＝＞　");
+    fflush(stdout);
     scanf("%d", &high);
     printf("盤面の幅を入力＝＞　");
+    fflush(stdout);
     scanf("%d", &wide);
 
-
     //配列初期化
-     for(y = 0; y <= high + 1; y++) { 
-        for(x = 0; x <= wide + 1; x++) { 
-            map[x][y][0] = map[x][y][1] = -1; 
-        } 
-    } 
+     for(y = 0; y <= high + 1; y++) {
+        for(x = 0; x <= wide + 1; x++) {
+            map[x][y][0] = map[x][y][1] = -1;
+        }
+    }
 
     do{
         printf("爆弾の数(%d個以内)を入力＝＞　",high*wide);
+        fflush(stdout);
         scanf("%d", &bombs);
     }while(bombs > high*wide || bombs < 0);
 
     //爆弾セット
     srand( time(0) );
-    for(x = 0; x < bombs;) { 
+    for(x = 0; x < bombs;) {
         ranx = rand() % wide + 1;
         rany = rand() % high + 1;
         if(map[ranx][rany][0] != -2) {
-            map[ranx][rany][0] = -2; 
+            map[ranx][rany][0] = -2;
             x++;
         }
     }
-    
-    /* for (y = 1; y <= high; y++) { 
-        for (x = 1; x <= wide; x++){
-            printf("%d ",map[x][y][0]);
-        }
-        puts("");
-    } puts("\n"); */
 
 
     //場所のまわりの爆弾を数える
     for (ys = 1; ys <= high; ys++){
         for(xs = 1; xs <= wide ; xs++){
             if(map[xs][ys][0] == -2){
-                map[xs][ys][2] = -2; 
+                map[xs][ys][2] = -2;
                 continue;
             }
-            bomb = 0; 
+            bomb = 0;
 
-            if (map[xs - 1][ys - 1][0] == -2) 
-                bomb ++; 
-            if (map[xs - 1][ys][0] == -2) 
-                bomb ++; 
-            if (map[xs - 1][ys + 1][0] == -2) 
-                bomb ++; 
-            if (map[xs][ys - 1][0] == -2) 
-                bomb ++; 
-            if (map[xs][ys + 1][0] == -2) 
-                bomb ++; 
-            if (map[xs + 1][ys - 1][0] == -2) 
-                bomb ++; 
-            if (map[xs + 1][ys][0] == -2) 
-                bomb ++; 
-            if (map[xs + 1][ys + 1][0] == -2) 
-                bomb ++; 
+            if (map[xs - 1][ys - 1][0] == -2)
+                bomb ++;
+            if (map[xs - 1][ys][0] == -2)
+                bomb ++;
+            if (map[xs - 1][ys + 1][0] == -2)
+                bomb ++;
+            if (map[xs][ys - 1][0] == -2)
+                bomb ++;
+            if (map[xs][ys + 1][0] == -2)
+                bomb ++;
+            if (map[xs + 1][ys - 1][0] == -2)
+                bomb ++;
+            if (map[xs + 1][ys][0] == -2)
+                bomb ++;
+            if (map[xs + 1][ys + 1][0] == -2)
+                bomb ++;
 
-            map[xs][ys][2] = bomb; 
+            map[xs][ys][2] = bomb;
         }
     }
 
-    /* for (y = 1; y <= high; y++) { 
-        for (x = 1; x <= wide; x++){
-            printf("%2d ",map[x][y][2]);
-        }
-        puts("");
-    } puts("\n"); */
+    //neta(1);
+    puts("");
 
 
-    //ゲーム開始 
-    while (1) { 
+    //ゲーム開始
+    while (1) {
         n = 0;
-         //現在の結果を画面に表示 
-        printf("  ");
-        for(x = 1; x <= wide; x++){
-            printf(" %d",x % 10);
-        }
-        printf("\n +"); 
-        for(x = 1; x <= wide; x++){
-            printf("--");
-        }
-        puts("-+"); 
-        for (y = 1; y <= high; y++) { 
-            printf("%d| ", y % 10);
-
-            for (x = 1; x <= wide; x++) { 
-                if (map[x][y][1] == 1) { 
-                    if(map[x][y][2] == 1)
-                        printf("\033[038;2;100;100;255m"); 
-                    else if(map[x][y][2] == 2)
-                        printf("\033[038;2;100;255;100m");
-                    else if(map[x][y][2] == 3)
-                        printf("\033[038;2;255;100;100m");
-                    else if(map[x][y][2] == 4)
-                        printf("\033[038;2;255;100;255m");
-                    else if(map[x][y][2] == 5)
-                        printf("\033[038;2;165;42;42m");
-                    else if(map[x][y][2] == 6)
-                        printf("\033[038;2;0;206;100m");
-                    else if(map[x][y][2] == 7)
-                        printf("\033[038;2;10;10;10m");
-                    else if(map[x][y][2] == 8)
-                        printf("\033[038;2;127;127;127m");
-                    printf("%d \033[m",map[x][y][2]); 
-                } else { 
-                    printf("  "); 
-                    n++;
-                } 
-            } 
-            puts("|"); 
-        } 
-        
-        printf(" +"); 
-        for(x = 1; x <= wide; x++){
-            printf("--");
-        }
-        puts("-+"); 
+         //現在の結果を画面に表示
+        n = neta(0);
 
         if(bombs == n){
             fin = 1;
             break;
         }
 
-         //プレーヤーが場所を入力  
-        count++; 
-        printf("場所を入力してください (x y) [%d 回目]:", count); 
+         //プレーヤーが場所を入力
+        count++;
+        printf("場所を入力してください (x y) [%d 回目]:", count);
+        fflush(stdout);
         scanf("%d %d",&xs ,&ys);
 
          //爆弾を引いた
-        if (map[xs][ys][0] == -2) 
-            break; 
-        
+        if (map[xs][ys][0] == -2)
+            break;
+
         opensel(xs, ys);
-    } 
+    }
      //ゲーム終了
     if(fin == 0){
-        printf("爆弾 !\n"); 
-    
-        for (y = 1; y <= high; y++) { 
-            for (x = 1; x <= wide; x++){
-                printf("%2d ",map[x][y][0]);
-            }
-            puts("");
-        } 
+        printf("爆弾 !\n");
+        neta(1);
     }
 
     if(fin == 1){
         puts("congratulation!");
-    } 
+        fflush(stdout);
+    }
     return 0;
+}
+
+int neta(int s){
+    int n = 0, x, y;
+    printf("  ");
+    for(x = 1; x <= wide; x++)
+        printf(" %d",x % 10);
+    printf("\n +");
+    for(x = 1; x <= wide; x++)
+        printf("--");
+    puts("-+");
+    for (y = 1; y <= high; y++) {
+        printf("%d| ", y % 10);
+
+        for (x = 1; x <= wide; x++) {
+            if (map[x][y][1] == 1) {
+                if(map[x][y][2] == 1)
+                    printf("\033[038;2;100;100;255m");
+                else if(map[x][y][2] == 2)
+                    printf("\033[038;2;100;255;100m");
+                else if(map[x][y][2] == 3)
+                    printf("\033[038;2;255;100;100m");
+                else if(map[x][y][2] == 4)
+                    printf("\033[038;2;255;100;255m");
+                else if(map[x][y][2] == 5)
+                    printf("\033[038;2;165;42;42m");
+                else if(map[x][y][2] == 6)
+                    printf("\033[038;2;0;206;100m");
+                else if(map[x][y][2] == 7)
+                    printf("\033[038;2;10;10;10m");
+                else if(map[x][y][2] == 8)
+                    printf("\033[038;2;127;127;127m");
+                printf("%d \033[m",map[x][y][2]);
+            } else if (s == 0) {
+                printf("  ");
+                n++;
+            } else if (s == 1){
+                if(map[x][y][0] == -2)
+                    printf("B ");
+                else
+                    printf("%d ",map[x][y][2]);
+            }
+        }
+        puts("|");
+    }
+
+    printf(" +");
+    for(x = 1; x <= wide; x++){
+        printf("--");
+    }
+    puts("-+");
+    fflush(stdout);
+    return n;
 }
 
 void opensel(int x,int y){
